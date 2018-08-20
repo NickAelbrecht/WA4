f2b38de06b2fee3c0bc22f50c190f9c49c7da658
NickAelbrecht <nick.aelbrecht.y9727@student.hogent.be>
2018-08-20 17:36:26 +0200

    kjhfg

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: true
 };
 
-export const BACKEND_URL = "https://sportapp-backend1.herokuapp.com";
+export const BACKEND_URL = "https://sportapp-backend1.herokuapp.com/";
